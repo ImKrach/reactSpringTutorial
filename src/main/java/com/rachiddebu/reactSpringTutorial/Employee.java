17b6896c1917fb945e1cb2e04821f125cb90e7b9
ImKrach <rachid.debu@yahoo.fr>
2018-06-04 15:51:52 +0200

    Premier commit

---
+++ src/main/java/com/rachiddebu/reactSpringTutorial/Employee.java
@@ -0,0 +1,21 @@
+package com.rachiddebu.reactSpringTutorial;
+
+import javax.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Employee {
+	private @Id @GeneratedValue Long id;
+	private String firstName;
+	private String lastName;
+	private String description;
+
+	private Employee() {}
+
+	public Employee(String firstName, String lastName, String description) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.description = description;
+	}
+}
